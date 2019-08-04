f8b914d6d6b5ded34fb8a31520cd627416edc8e0
Jose Carlos Alvarez <53585057+tentale1@users.noreply.github.com>
2019-08-04 11:44:49 -0500

    Update Eficiencia.java

--- Eficiencia.java
+++ Eficiencia.java
@@ -10,7 +10,7 @@ public class Eficiencia
 {
     public static void main(String[] args)
     {
-        double gal=0,mill=0,mpg,lpk;
+        double gal,mill,mpg,lpk;
         
         Scanner Teclado = new Scanner (System.in);
         
